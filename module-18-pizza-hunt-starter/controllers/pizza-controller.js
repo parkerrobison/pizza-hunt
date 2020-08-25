554dba88915c7e756895acfdefc802b4397dde7b
probison <parker.robison@hotmail.com>
2020-08-24 22:06:34 -0600

    added routes/controllers for pizzas

---
+++ module-18-pizza-hunt-starter/controllers/pizza-controller.js
@@ -0,0 +1,65 @@
+const { pizza, Pizza } = require('../models');
+const { db } = require('../models/Pizza');
+
+const pizzaController = {
+    // get all pizzas
+    getAllPizza(req, res) {
+        Pizza.find({})
+        .then(dbPizzaData => res.json(dbPizzaData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+    },
+
+    //get one pizza by id
+    getPizzaById({ params }, res) {
+        Pizza.findOne({ _id: params.id })
+        .then(dbPizzaData => {
+            // If no pizza is found, send 404
+            if(!dbPizzaData) {
+                res.status(404).json({ message: "No pizza was found with this id!" });
+                return;
+            }
+            res.json(dbPizzaData)
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(400).json(err);
+        });
+    },
+    //create Pizza
+    createPizza({ body }, res) {
+        Pizza.create(body)
+        .then(dbPizzaData => res.json(dbPizzaData))
+        .catch(err => res.status(400).json(err));
+    },
+    //update Pizza by id
+    updatePizza({ params, body }, res) {
+        // the third parameter returns the new version of the document. Without it we would get the old version.
+        Pizza.findOneAndUpdate({ _id: params.id}, body, { new: true })
+        .then(dbPizzaData => {
+            if (!dbPizzaData) {
+                res.status(404).json({ message: "No pizza is found with this id!" });
+                return;
+            }
+            res.json(dbPizzaData);
+        })
+        .catch(err => res.status(400).json(err));
+    },
+
+    //delete Pizza
+    deletePizza({ params }, res) {
+        Pizza.findOneAndDelete({ _id: params.id })
+        .then(dbPizzaData => {
+            if(!dbPizzaData) {
+                res.status(404).json({ message: 'No pizza found with this id! '});
+                return;
+            }
+            res.json(dbPizzaData);
+        })
+        .catch(err => res.status(400).json(err));
+    }
+};
+
+module.exports = pizzaController;
