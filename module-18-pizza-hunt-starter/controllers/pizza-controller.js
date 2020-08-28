49e9dfd5f0ff1628c27deb4d3e3cebe5eefdf447
probison <parker.robison@hotmail.com>
2020-08-27 22:59:29 -0600

    start of validation

--- module-18-pizza-hunt-starter/controllers/pizza-controller.js
+++ module-18-pizza-hunt-starter/controllers/pizza-controller.js
@@ -48,7 +48,7 @@ const pizzaController = {
     //update Pizza by id
     updatePizza({ params, body }, res) {
         // the third parameter returns the new version of the document. Without it we would get the old version.
-        Pizza.findOneAndUpdate({ _id: params.id}, body, { new: true })
+        Pizza.findOneAndUpdate({ _id: params.id}, body, { new: true, runValidators: true })
         .then(dbPizzaData => {
             if (!dbPizzaData) {
                 res.status(404).json({ message: "No pizza is found with this id!" });
